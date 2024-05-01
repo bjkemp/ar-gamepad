bc161ed30675e88f0977fdd6794e417893d1e5c5
Benjamin Kemp <bjkemp@gmail.com>
2024-05-01 09:08:20 -0400

    Remove Counter component import from App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,4 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
   import ArGamePad from './lib/ARGamePad.svelte';
 </script>
 
